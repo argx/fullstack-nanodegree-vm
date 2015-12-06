814e2a85f2fed07021c5e0f44c0c5b78eb4d99bc
Argenis Arriojas <argearriojas@gmail.com>
2015-12-05 23:45:57 -0430

    Add table schemas: player and match

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,38 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+drop table if exists match;
+drop table if exists player;
 
+create table player (
+	id				serial primary key,
+	name			varchar(64)
+);
+
+create table match (
+	id 				serial primary key,
+	winner			integer references player(id),
+	loser			integer references player(id)
+);
+
+insert into player (name, lastname) values
+	('Lionel Messi'),
+	('Cristiano Ronaldo'),
+	('Luis Suárez'),
+	('Gareth Bale'),
+	('Andrés Iniesta'),
+	('Neymar Santos'),
+	('Arturo Vidal'),
+	('Arjen Robben'),
+	('Zlatan Ibrahimović'),
+	('Ángel di María'),
+	('Manuel Neuer'),
+	('Toni Kroos'),
+	('Thiago Silva'),
+	('Marco Reus'),
+	('Sergio Busquets'),
+	('James Rodríguez'),
+	('Frank Ribéry'),
+	('Sergio Agüero'),
+	('Thomas Müller'),
+	('Philipp Lahm');
